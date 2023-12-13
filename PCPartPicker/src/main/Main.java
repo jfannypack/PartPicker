169200ee1c795d1b5ea2b2c7c65203231d3074ee
jfanjfan <112498961+jfanjfan@users.noreply.github.com>
2023-12-13 14:10:59 -0500

    budget tracker

--- PCPartPicker/src/main/Main.java
+++ PCPartPicker/src/main/Main.java
@@ -421,148 +421,154 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         Random random = new Random();
+        while (true){
+            String buildType = getBuildType(scanner);
 
-        String buildType = getBuildType(scanner);
+            // Create component factories
+            ComponentFactory cpuFactory = new CPUFactory();
+            ComponentFactory gpuFactory = new GPUFactory();
+            ComponentFactory ramFactory = new RAMFactory();
+            ComponentFactory moboFactory = new MOBOFactory();
+            ComponentFactory storageFactory = new STORAGEFactory();
 
-        // Create component factories
-        ComponentFactory cpuFactory = new CPUFactory();
-        ComponentFactory gpuFactory = new GPUFactory();
-        ComponentFactory ramFactory = new RAMFactory();
-        ComponentFactory moboFactory = new MOBOFactory();
-        ComponentFactory storageFactory = new STORAGEFactory();
-
-        // Create lists to store CPU, GPU, and RAM options
-        List<Component> cpuOptions = new ArrayList<>();
-        List<Component> gpuOptions = new ArrayList<>();
-        List<Component> ramOptions = new ArrayList<>();
-        List<Component> moboOptions = new ArrayList<>();
-        List<Component> compatibleMoboOptions = new ArrayList<>();
-        List<Component> storageOptions = new ArrayList<>();
-        List<Component> filteredCpuOptions = new ArrayList<>();
-        List<Component> filteredGpuOptions = new ArrayList<>();
-
-
-        // Create CPU options with random GHz speeds
-        cpuOptions.add(cpuFactory.createComponent("Intel", "i7 12700K", "Gaming",3.6, 279.99, 0, "LGA 1700", "", "0"));
-        cpuOptions.add(cpuFactory.createComponent("Intel", "i3 13100", "Productivity",3.4, 147.99, 0, "LGA 1700", "", "0"));
-        cpuOptions.add(cpuFactory.createComponent("Intel", "Celeron G5925", "Productivity",3.6, 60.00, 0, "LGA 1200", "", "0"));
-        cpuOptions.add(cpuFactory.createComponent("AMD", "Ryzen 5 3600  ", "Gaming",3.6, 119.99, 0, "AM5", "", "0"));
-        cpuOptions.add(cpuFactory.createComponent("AMD", "Ryzen 5 7600X  ", "Gaming",4.7, 218.99, 0, "AM5", "", "0"));
-        cpuOptions.add(cpuFactory.createComponent("AMD", "Ryzen Threadripper PRO 5955WX", "Productivity",4.0, 1034.99, 0, "sWRX8", "", "0"));
-
-        // Create GPU options
-        gpuOptions.add(gpuFactory.createComponent("NVIDIA", "GeForce RTX 3080", "Gaming",0, 699.00, 0, "", "", "0"));
-        gpuOptions.add(gpuFactory.createComponent("AMD", "Radeon RX 6900 XT", "Gaming",0, 999.00, 0, "", "", "0"));
-        gpuOptions.add(gpuFactory.createComponent("NVIDIA", "GeForce RTX 3060", "Gaming",0, 299.00, 0, "", "", "0"));
-        gpuOptions.add(gpuFactory.createComponent("NVIDIA", "RTX Titan", "Productivity",0, 1599.99, 0, "", "", "0"));
-
-        // Create RAM options
-        ramOptions.add(ramFactory.createComponent("Corsair", "Vengeance", "Gaming",0, 345, 16, "", "", "0"));
-        ramOptions.add(ramFactory.createComponent("G.SKILL", "DDR5", "Gaming", 0, 80, 16, "", "", "0"));
+            // Create lists to store CPU, GPU, and RAM options
+            List<Component> cpuOptions = new ArrayList<>();
+            List<Component> gpuOptions = new ArrayList<>();
+            List<Component> ramOptions = new ArrayList<>();
+            List<Component> moboOptions = new ArrayList<>();
+            List<Component> compatibleMoboOptions = new ArrayList<>();
+            List<Component> storageOptions = new ArrayList<>();
+            List<Component> filteredCpuOptions = new ArrayList<>();
+            List<Component> filteredGpuOptions = new ArrayList<>();
 
 
-        moboOptions.add(moboFactory.createComponent("ASUS", "B550", "Gaming",0, 99.99, 0, "AM5", "", "0"));
-        moboOptions.add(moboFactory.createComponent("ASUS", "Pro WS WRX80E-SAGE", "Productivity",0, 999.00, 0, "sWRX8", "", "0"));
-        moboOptions.add(moboFactory.createComponent("ASRock", "Z90", "Gaming",0, 109.99, 0, "LGA 1700", "", "0"));
-        moboOptions.add(moboFactory.createComponent("ASRock", "B660M PRO RS", "Gaming",0, 89.99, 0, "LGA 1700", "", "0"));
-        moboOptions.add(moboFactory.createComponent("GIGABYTE", "Z790 GAMING X AX", "Gaming",0, 229.99, 0, "LGA 1700", "", "0"));
+            // Create CPU options with random GHz speeds
+            cpuOptions.add(cpuFactory.createComponent("Intel", "i7 12700K", "Gaming",3.6, 279.99, 0, "LGA 1700", "", "0"));
+            cpuOptions.add(cpuFactory.createComponent("Intel", "i3 13100", "Productivity",3.4, 147.99, 0, "LGA 1700", "", "0"));
+            cpuOptions.add(cpuFactory.createComponent("Intel", "Celeron G5925", "Productivity",3.6, 60.00, 0, "LGA 1200", "", "0"));
+            cpuOptions.add(cpuFactory.createComponent("AMD", "Ryzen 5 3600  ", "Gaming",3.6, 119.99, 0, "AM5", "", "0"));
+            cpuOptions.add(cpuFactory.createComponent("AMD", "Ryzen 5 7600X  ", "Gaming",4.7, 218.99, 0, "AM5", "", "0"));
+            cpuOptions.add(cpuFactory.createComponent("AMD", "Ryzen Threadripper PRO 5955WX", "Productivity",4.0, 1034.99, 0, "sWRX8", "", "0"));
+
+            // Create GPU options
+            gpuOptions.add(gpuFactory.createComponent("NVIDIA", "GeForce RTX 3080", "Gaming",0, 699.00, 0, "", "", "0"));
+            gpuOptions.add(gpuFactory.createComponent("AMD", "Radeon RX 6900 XT", "Gaming",0, 999.00, 0, "", "", "0"));
+            gpuOptions.add(gpuFactory.createComponent("NVIDIA", "GeForce RTX 3060", "Gaming",0, 299.00, 0, "", "", "0"));
+            gpuOptions.add(gpuFactory.createComponent("NVIDIA", "RTX Titan", "Productivity",0, 1599.99, 0, "", "", "0"));
+
+            // Create RAM options
+            ramOptions.add(ramFactory.createComponent("Corsair", "Vengeance", "Gaming",0, 345, 16, "", "", "0"));
+            ramOptions.add(ramFactory.createComponent("G.SKILL", "DDR5", "Gaming", 0, 80, 16, "", "", "0"));
 
 
-        storageOptions.add(storageFactory.createComponent("Samsung", "870 EVO", "Productivity",0, 74.99, 0, "", "SSD", "1TB"));
-        storageOptions.add(storageFactory.createComponent("AORUS", "GEN4 5000E", "Gaming",0, 89.99, 0, "", "SSD", "2TB"));
-        storageOptions.add(storageFactory.createComponent("Kingston", "A400", "Gaming",0, 24.49, 0, "", "SSD", "256GB"));
+            moboOptions.add(moboFactory.createComponent("ASUS", "B550", "Gaming",0, 99.99, 0, "AM5", "", "0"));
+            moboOptions.add(moboFactory.createComponent("ASUS", "Pro WS WRX80E-SAGE", "Productivity",0, 999.00, 0, "sWRX8", "", "0"));
+            moboOptions.add(moboFactory.createComponent("ASRock", "Z90", "Gaming",0, 109.99, 0, "LGA 1700", "", "0"));
+            moboOptions.add(moboFactory.createComponent("ASRock", "B660M PRO RS", "Gaming",0, 89.99, 0, "LGA 1700", "", "0"));
+            moboOptions.add(moboFactory.createComponent("GIGABYTE", "Z790 GAMING X AX", "Gaming",0, 229.99, 0, "LGA 1700", "", "0"));
 
 
-        for (Component cpu : cpuOptions) {
-            if (cpu.getCategory().equalsIgnoreCase(buildType)) {
-                filteredCpuOptions.add(cpu);
+            storageOptions.add(storageFactory.createComponent("Samsung", "870 EVO", "Productivity",0, 74.99, 0, "", "SSD", "1TB"));
+            storageOptions.add(storageFactory.createComponent("AORUS", "GEN4 5000E", "Gaming",0, 89.99, 0, "", "SSD", "2TB"));
+            storageOptions.add(storageFactory.createComponent("Kingston", "A400", "Gaming",0, 24.49, 0, "", "SSD", "256GB"));
+
+
+            for (Component cpu : cpuOptions) {
+                if (cpu.getCategory().equalsIgnoreCase(buildType)) {
+                    filteredCpuOptions.add(cpu);
+                }
             }
-        }
 
-        for (Component gpu : gpuOptions) {
-            if (gpu.getCategory().equalsIgnoreCase(buildType)) {
-                filteredGpuOptions.add(gpu);
+            for (Component gpu : gpuOptions) {
+                if (gpu.getCategory().equalsIgnoreCase(buildType)) {
+                    filteredGpuOptions.add(gpu);
+                }
             }
-        }
 
-        double budget = getBudgetInput(scanner);
+            double budget = getBudgetInput(scanner);
 
-        // Ask the user to select one CPU
-        System.out.println("Select one CPU:");
-        for (int i = 0; i < filteredCpuOptions.size(); i++) {
-            System.out.println(i + 1 + ". " + filteredCpuOptions.get(i).getName());
-        }
-
-        int selectedCpuIndex = getValidInput(1, filteredCpuOptions.size(), "Enter the number corresponding to your choice:", scanner) - 1;
-        Component selectedCpu = filteredCpuOptions.get(selectedCpuIndex);
-
-        for (Component mobo : moboOptions) {
-            if (selectedCpu.getSocket().equals(mobo.getSocket())) {
-                compatibleMoboOptions.add(mobo);
+            // Ask the user to select one CPU
+            System.out.println("Select one CPU:");
+            for (int i = 0; i < filteredCpuOptions.size(); i++) {
+                System.out.println(i + 1 + ". " + filteredCpuOptions.get(i).getName());
             }
+
+            int selectedCpuIndex = getValidInput(1, filteredCpuOptions.size(), "Enter the number corresponding to your choice:", scanner) - 1;
+            Component selectedCpu = filteredCpuOptions.get(selectedCpuIndex);
+
+            for (Component mobo : moboOptions) {
+                if (selectedCpu.getSocket().equals(mobo.getSocket())) {
+                    compatibleMoboOptions.add(mobo);
+                }
+            }
+            System.out.println("Select one MOBO:");
+            for (int i = 0; i < compatibleMoboOptions.size(); i++) {
+                System.out.println(i + 1 + ". " + compatibleMoboOptions.get(i).getName());
+            }
+            int selectedMoboIndex = getValidInput(1, compatibleMoboOptions.size(), "Enter the number corresponding to your choice:", scanner) - 1;
+            Component selectedMobo = compatibleMoboOptions.get(selectedMoboIndex);
+
+
+            // Ask the user to select one GPU
+            System.out.println("Select one GPU:");
+            for (int i = 0; i < filteredGpuOptions.size(); i++) {
+                System.out.println(i + 1 + ". " + filteredGpuOptions.get(i).getName());
+            }
+
+            int selectedGpuIndex = getValidInput(1, filteredGpuOptions.size(), "Enter the number corresponding to your choice:", scanner) - 1;
+            Component selectedGpu = filteredGpuOptions.get(selectedGpuIndex);
+
+            System.out.println("Select one Storage:");
+            for (int i = 0; i < storageOptions.size(); i++) {
+                System.out.println(i + 1 + ". " + storageOptions.get(i).getName() + storageOptions.get(i).getStorageSize());
+            }
+            int selectedStorageIndex = getValidInput(1, storageOptions.size(), "Enter the number corresponding to your choice:", scanner) - 1;
+            Component selectedStorage = storageOptions.get(selectedStorageIndex);
+
+            // Ask the user to select one RAM
+            System.out.println("Select one RAM:");
+            for (int i = 0; i < ramOptions.size(); i++) {
+                System.out.println(i + 1 + ". " + ramOptions.get(i).getName() + " " + ramOptions.get(i).getRamSize() + "GB");
+            }
+
+            int selectedRamIndex = getValidInput(1, ramOptions.size(), "Enter the number corresponding to your choice:", scanner) - 1;
+            Component selectedRam = ramOptions.get(selectedRamIndex);
+
+            // Ask the user how many sets of RAM they want
+            int numRamSets = getValidInput(1, Integer.MAX_VALUE, "Enter the number of RAM sets you want:", scanner);
+
+            // Create a PC configuration
+            PCConfiguration configuration = new PCConfiguration();
+            configuration.addComponent(selectedCpu);
+            configuration.addComponent(selectedGpu);
+            configuration.addComponent(selectedMobo);
+            configuration.addComponent((selectedStorage));
+
+            // Add the specified number of RAM sets
+            for (int i = 0; i < numRamSets; i++) {
+                configuration.addComponent(selectedRam);
+            }
+
+            // Use the Strategy pattern to check compatibility for gaming
+            double totalPrice = configuration.calculateTotalPrice();
+            CompatibilityChecker gamingChecker = new GamingCompatibilityChecker();
+            boolean isGamingCompatible = configuration.checkCompatibility(gamingChecker);
+            boolean isBudgetCompatible = gamingChecker.isWithinBudget(budget, totalPrice);
+
+            if (isBudgetCompatible) {
+                // Display selected components and exit the loop
+                System.out.println("Gaming Compatibility: " + isGamingCompatible);
+                System.out.println("Budget Compatibility: " + isBudgetCompatible);
+                System.out.println();
+                System.out.println("Total Price: $" + totalPrice);
+                printComputerASCIIArt(configuration);
+                break; // Exit the loop
+            } else {
+                // Display a message and prompt the user to try again
+                System.out.println("The selected components exceed your budget by $" + (totalPrice - budget) + ". Please try again.");
+
+            }
+
         }
-        System.out.println("Select one MOBO:");
-        for (int i = 0; i < compatibleMoboOptions.size(); i++) {
-            System.out.println(i + 1 + ". " + compatibleMoboOptions.get(i).getName());
-        }
-        int selectedMoboIndex = getValidInput(1, compatibleMoboOptions.size(), "Enter the number corresponding to your choice:", scanner) - 1;
-        Component selectedMobo = compatibleMoboOptions.get(selectedMoboIndex);
-
-
-        // Ask the user to select one GPU
-        System.out.println("Select one GPU:");
-        for (int i = 0; i < filteredGpuOptions.size(); i++) {
-            System.out.println(i + 1 + ". " + filteredGpuOptions.get(i).getName());
-        }
-
-        int selectedGpuIndex = getValidInput(1, filteredGpuOptions.size(), "Enter the number corresponding to your choice:", scanner) - 1;
-        Component selectedGpu = filteredGpuOptions.get(selectedGpuIndex);
-
-        System.out.println("Select one Storage:");
-        for (int i = 0; i < storageOptions.size(); i++) {
-            System.out.println(i + 1 + ". " + storageOptions.get(i).getName() + storageOptions.get(i).getStorageSize());
-        }
-        int selectedStorageIndex = getValidInput(1, storageOptions.size(), "Enter the number corresponding to your choice:", scanner) - 1;
-        Component selectedStorage = storageOptions.get(selectedStorageIndex);
-
-        // Ask the user to select one RAM
-        System.out.println("Select one RAM:");
-        for (int i = 0; i < ramOptions.size(); i++) {
-            System.out.println(i + 1 + ". " + ramOptions.get(i).getName() + " " + ramOptions.get(i).getRamSize() + "GB");
-        }
-
-        int selectedRamIndex = getValidInput(1, ramOptions.size(), "Enter the number corresponding to your choice:", scanner) - 1;
-        Component selectedRam = ramOptions.get(selectedRamIndex);
-
-        // Ask the user how many sets of RAM they want
-        int numRamSets = getValidInput(1, Integer.MAX_VALUE, "Enter the number of RAM sets you want:", scanner);
-
-        // Create a PC configuration
-        PCConfiguration configuration = new PCConfiguration();
-        configuration.addComponent(selectedCpu);
-        configuration.addComponent(selectedGpu);
-        configuration.addComponent(selectedMobo);
-        configuration.addComponent((selectedStorage));
-
-        // Add the specified number of RAM sets
-        for (int i = 0; i < numRamSets; i++) {
-            configuration.addComponent(selectedRam);
-        }
-
-        // Use the Strategy pattern to check compatibility for gaming
-        double totalPrice = configuration.calculateTotalPrice();
-        CompatibilityChecker gamingChecker = new GamingCompatibilityChecker();
-        boolean isGamingCompatible = configuration.checkCompatibility(gamingChecker);
-        boolean isBudgetCompatible = gamingChecker.isWithinBudget(budget, totalPrice);
-
-
-        System.out.println("Gaming Compatibility: " + isGamingCompatible);
-        System.out.println("Budget Compatibility: " + isBudgetCompatible);
-        System.out.println();
-
-        // Display selected components
-        configuration.displayComponents();
-        System.out.println("Total Price: $" + totalPrice);
-        printComputerASCIIArt(configuration);
     }
 }
